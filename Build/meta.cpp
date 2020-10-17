aac648f3690ed126e6b9d5a39d8888f774b01b93
Werthles <armawerthles@gmail.com>
2020-10-17 17:21:29 +0100

    v2.1

---
+++ Build/meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 510031102;
+name = "Werthles' Headless Module";
+timestamp = 5247654717000964538;
---
+++ Build/mod.cpp
@@ -0,0 +1,17 @@
+dir = "@Werthles' Headless Module";
+author = "Werthles";
+hideName = 0;
+hidePicture = 0;
+description = "http://steamcommunity.com/sharedfiles/filedetails/?id=459917508";
+logo = "\Werthles_WHK\data\iconWHMColour_ca.paa"; //mod icon when not hovered over and in editor before placing
+logoOver = "\Werthles_WHK\data\iconWHMColourLarge_ca.paa"; //mod icon when hovered over
+tooltip = "Werthles' Headless Module"; //word when mod icon hovered over
+name = "Werthles' Headless Module"; // name in expansions
+picture = "\Werthles_WHK\data\iconWHMExpansions_ca.paa"; // picture in expansions
+overview = "Werthles' Headless Module<br/><br/>The easy way to use headless clients to control AI.<br/><br/>1) Place a WH Module and named, playable headless clients.<br/>2) Connect Headless Clients to your server.<br/>3) Play your mission!"; //description in expansion menu
+actionName = "WHK Link"; //instead of purchase in expansions
+action = "https://forums.bistudio.com/topic/184485-werthles-headless-module/"; //where WHK Link goes
+tooltipOwned = "WHK Available";
+overviewPicture = "\Werthles_WHK\data\iconWHK_ca.paa";
+overviewText = "WHK - The easy way to setup a headless client mission.";
+overviewFootnote = "If you want to release this as part of your mod (not a mission scenario), speak to me (Werthles) first!";
